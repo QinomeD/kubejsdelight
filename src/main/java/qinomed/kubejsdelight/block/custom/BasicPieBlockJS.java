06938bb3381e3f67a8c52f67427866e19c287e69
QinomeD <cpueater@interia.pl>
2026-02-12 19:16:18 +0100

    custom bite sizes, phenomenal

--- src/main/java/qinomed/kubejsdelight/block/custom/BasicPieBlockJS.java
+++ src/main/java/qinomed/kubejsdelight/block/custom/BasicPieBlockJS.java
@@ -7,11 +7,20 @@ import vectorwing.farmersdelight.common.block.PieBlock;
 import java.util.function.Supplier;
 
 public class BasicPieBlockJS extends PieBlock {
+    protected final int bites;
+
     public BasicPieBlockJS(PieBlockBuilder builder) {
         super(builder.createProperties(), () -> BuiltInRegistries.ITEM.get(builder.sliceItem));
+        bites = builder.bites;
+    }
+
+    @Override
+    public int getMaxBites() {
+        return bites;
     }
 
     public BasicPieBlockJS(Properties properties, Supplier<Item> pieSlice) {
         super(properties, pieSlice);
+        bites = 4;
     }
 }
